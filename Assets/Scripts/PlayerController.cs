90202266610cebe9d496b1b666721f77a326a0f2
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-01-12 00:06:20 +0100

    Add Scripting Animation & Flipping Sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        FlipSprite(horizontalInput);
+
+        _animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+    }
+
+    void FlipSprite(float horizontalInput)
+    {
+        if (horizontalInput < 0 && !_spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (horizontalInput > 0 && _spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = false;
+        }
     }
 }
