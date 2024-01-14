4cfa9ef0c1526c598c3c81680749c5f6995e24c3
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-01-14 17:04:39 +0100

    Vertical input & Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,19 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
 
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
     private BoxCollider2D _boxCollider2D;
+    private Rigidbody2D _rigidbody2D;
 
     private bool _isJumping;
     private const float standingOffsetX = 0.015f;
     private const float standingOffsetY = 1f;
     private const float standingSizeX = 0.7f;
     private const float standingSizeY = 2.13f;
-    
+
     private const float crouchingOffsetX = -0.11f;
     private const float crouchingOffsetY = 0.6f;
     private const float crouchingSizeX = 0.94f;
@@ -24,55 +26,56 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        float verticalInput = Input.GetAxisRaw("Jump");
+
+        Move(horizontalInput);
+        
 
         FlipSprite(horizontalInput);
-        MovePlayer(horizontalInput, verticalInput);
-
-        _animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
-        _animator.SetFloat("Height", verticalInput);
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
             _animator.SetBool("IsCrouching", true);
-
-            _boxCollider2D.size = new Vector2(crouchingSizeX, crouchingSizeY);
-            _boxCollider2D.offset = new Vector2(crouchingOffsetX, crouchingOffsetY);
-        } 
+            UpdateColliderSizeAndOffset(crouchingSizeX, crouchingSizeY, crouchingOffsetX, crouchingOffsetY);
+        }
         else
         {
             _animator.SetBool("IsCrouching", false);
-
-            _boxCollider2D.size = new Vector2(standingSizeX, standingSizeY);
-            _boxCollider2D.offset = new Vector2(standingOffsetX, standingOffsetY);
+            UpdateColliderSizeAndOffset(standingSizeX, standingSizeY, standingOffsetX, standingOffsetY);
         }
 
+        if (verticalInput > 0 && !_isJumping)
+        {
+            Jump();
+        }
     }
 
-    void MovePlayer(float horizontal, float vertical)
+    private void Jump()
+    {
+        _isJumping = true;
+        _rigidbody2D.AddForce((Vector2.up * _jumpForce), ForceMode2D.Force);
+        _animator.SetBool("IsJumping", true);
+    }
+
+    void Move(float horizontal)
     {
         var position = transform.position;
         position.x += (horizontal * _speed) * Time.deltaTime;
+        _animator.SetFloat("Speed", Mathf.Abs(horizontal));
         transform.position = position;
+    }
 
-        if (vertical > 0 && !_isJumping)
-        {
-            _isJumping = true;
-            _animator.SetBool("IsJumping", true);
-        }
+    void UpdateColliderSizeAndOffset(float sizeX, float sizeY, float offsetX, float offsetY)
+    {
+        _boxCollider2D.size = new Vector2(sizeX, sizeY);
+        _boxCollider2D.offset = new Vector2(offsetX, offsetY);
     }
 
     void OnJumpAnimationEnd()
